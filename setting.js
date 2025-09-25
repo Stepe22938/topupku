4db958ba002f25dd2f51c317d6a4e5cf2b99ec54
Stepe22938 <123631481+Stepe22938@users.noreply.github.com>
2025-09-25 16:45:12 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -3,5 +3,6 @@
 global.username = "barmodssofficial"
 global.passwd = "barmodss123@"
 global.feenya = 50 // FEE PRODUK
-global.apikey = "la26f4f6jtuigvkb" // APIKEY DARI V-PEDIA
+global.apikey = "VPedia_cbs77vmmhwc98a30
+  " // APIKEY DARI V-PEDIA
 global.mongodb = "mongodb+srv://barrxxxx:barrxxxx@cluster0.pjozlkg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" // JANGAN DI UBAH INI DB
