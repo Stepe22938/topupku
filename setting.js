3c24d711ce9372548ba9d54368d2a5814eb009af
Stepe22938 <123631481+Stepe22938@users.noreply.github.com>
2025-09-25 17:57:42 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -3,5 +3,5 @@
 global.username = "Steve22938"
 global.passwd = "Steve22938"
 global.feenya = 50 // FEE PRODUK
-global.apikey = "n5g3yhvsvvwz6f36"
+global.apikey = "zytnnxb3jqswpfme"
 global.mongodb = "mongodb+srv://barrxxxx:barrxxxx@cluster0.pjozlkg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" // JANGAN DI UBAH INI DB
